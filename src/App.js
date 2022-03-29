4972e21c2a6b813c6b2ef6df600c844c956c5812
inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-03-29 02:24:52 +0100

    fixed footer component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 
-import {Switch,Route,Link} from 'react-router-dom'
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'
 import {Layout, Typography,Space} from 'antd'
-import {Navbar} from './components';
+import {Navbar,Exchanges,News,CryptoDetails,Cryptocurrencies,Homepage} from './components';
 import './App.css';
 
 function App() {
   return (
+    
     <div className='app'>
+      <Router>
       <div className="navbar">
         <Navbar />
 
       </div>
       <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes >
+            
+              <Route exact path="/" element={  <Homepage/>}/>
+              <Route exact path="/exchanges" element={  <Exchanges/>}/>
+              <Route exact path="/cryptocurrencies" element={  <Cryptocurrencies/>}/>
+              <Route exact path="/crypto/:coinId" element={  <CryptoDetails/>}/>
+              <Route exact path="/news" element={<News/>}/>
+              </Routes>
+            
+          </div>
+        </Layout>
 
-      </div>
       <div className="footer">
+        <Typography.Title  level={5} style={{color:'white',textAlign:'center'}}>
+          Cryptoverse <br />
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
 
       </div>
-
+      </div>
+      </Router>
       
     </div>
   );
